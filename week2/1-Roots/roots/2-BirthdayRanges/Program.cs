51a7ddd6e90a2e0639f13d401013d782f4890110
Vitosh <vitosh.doynov@gmail.com>
2015-06-17 22:45:35 +0300

    adding number 4

--- week2/1-Roots/roots/2-BirthdayRanges/Program.cs
+++ week2/1-Roots/roots/2-BirthdayRanges/Program.cs
@@ -1,23 +1,65 @@
 using System;
+using System.Collections.Generic;
+using System.Linq;
 
 class MainClass
 {
 	public static void Main ()
 	{
-		List <int> birthdays = new List<int> ();
-		birthdays = {1,1,1,3,4,5,6,6,6,6};
-		int[,] ranges =  {{1,100},{23,43},{3,300},{5,300},{32,45},{2,300}};
-		birthday_count(birthdays, ranges);
-	}
-	List<int> list = new List<int>();
-
-	//sort before you start
-	public static void birthday_count(int[] birthdaysOriginal, int[,]ranges)
-	{
-		int[] birthdays = {};
-		for (int i = 0; i <= 365; i++) 
+		List <int> liBirthdays = new List<int> (new int[]{1,1,1,3,4,5,6,6,6,6,7,10,100,100,300,300,301});
+		List<List<int>> liRanges = new List<List<int>> 
 		{
-			birthdays[i] = Array.Find(birthdaysOriginal, element => element == i);
+			new List<int>{1,100},   //14
+			new List<int>{9,100},   //3
+			new List<int>{100,100}, //2
+			new List<int>{300,302}, //3
+
+		};
+		birthday_count(liBirthdays, liRanges);
+	}
+
+	public static void birthday_count(List<int>liBirthdays, List<List<int>>liRanges)
+	{
+		Dictionary<int, int> myDictionary = new Dictionary<int,int> ();
+		List <int> liBirthdaysOrdered = new List<int> ();
+		List <int> liBirthdaysSum = new List<int> ();
+
+		int iResult = 0;
+		var liOriginal = liBirthdays.GroupBy (i => i);
+		int iDays = 367;
+		int zero = (int)0;
+		int iSum = 0;
+
+		foreach (var l in liOriginal) 
+		{
+			myDictionary.Add (l.Key, l.Count ());
 		}
+
+		for (int i = 0; i < iDays; i++) 
+		{
+			if (myDictionary.TryGetValue (i, out zero)) 
+			{
+				liBirthdaysOrdered.Add (myDictionary [i]);
+			} 
+			else 
+			{
+				liBirthdaysOrdered.Add (0);
+			}
+		}
+
+		for (int i = 0; i < liBirthdaysOrdered.Count(); i++) 
+		{
+			iSum += liBirthdaysOrdered [i];
+			liBirthdaysSum.Add (iSum);
+		}
+
+		foreach (var item in liRanges) {
+			int iBottom = liBirthdaysSum [item [0]-1];
+			int iTop = liBirthdaysSum [item [1]];
+			int position = iTop - iBottom;
+			iResult += position;
+		}
+		Console.WriteLine (iResult);
+
 	}
 }
---
+++ week2/1-Roots/roots/4-PhoneBook/PhoneBook.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+
+class MainClass
+{
+	public static void Main ()
+	{
+		var liPhoneBook = new List<Tuple<int,string>>();
+		liPhoneBook.Add(Tuple.Create(100, "Pesho"));
+		liPhoneBook.Add(Tuple.Create(200, "Gosho"));
+		liPhoneBook.Add(Tuple.Create(300, "Atanas"));
+		liPhoneBook.Add(Tuple.Create(400, "Az"));
+		liPhoneBook.Add(Tuple.Create(500, "Ivan"));
+		liPhoneBook.Add(Tuple.Create(600, "Ivan Petrov"));
+		liPhoneBook.Add(Tuple.Create(900, "Vitosh"));
+
+
+		List <int> liNumbers = new List<int> (new int[]{900, 300,400,100,600});
+		findNumbers (liNumbers, liPhoneBook);
+	}
+
+	public static void findNumbers(List<int> liNumbers, List<Tuple<int,string>> liPhoneBook)
+	{
+		liNumbers.Sort ();
+		liPhoneBook.Sort ();
+		int iPosition = 0;
+
+		foreach (var item in liNumbers) {
+			for (int i = iPosition; i < liPhoneBook.Count; i++) 
+			{
+				Console.WriteLine (i);
+				if (item == liPhoneBook[i].Item1) 
+				{
+					Console.WriteLine (liPhoneBook[i].Item2);
+					iPosition = i+1;
+					break;
+				}
+			}
+		}
+	}
+}
