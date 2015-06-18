e4c5969b42d1cfd36ef871684b26b30520d9256d
Stanislav Gatev <s2gatev@gmail.com>
2015-06-18 19:06:32 +0300

    Update k_min.py

--- week2/7-K-Min/k_min.py
+++ week2/7-K-Min/k_min.py
@@ -2,5 +2,6 @@ class KMin:
 
     # Finds the k-th minimum element in an unsorted collection.
     # numbers - [int]
-    def kthMinimum(numbers):
+    # k - int
+    def kthMinimum(numbers, k):
         pass
