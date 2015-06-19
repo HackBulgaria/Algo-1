5a4c3ea789297c06c9768fb7b61caba39c8fae44
Stanislav Gatev <s2gatev@gmail.com>
2015-06-19 12:03:44 +0300

    Add size to quadruplets.cpp

--- week2/3-Quadruplets/quadruplets.cpp
+++ week2/3-Quadruplets/quadruplets.cpp
@@ -2,7 +2,7 @@ class Quadruplets {
 public:
 
   // Returns the number of quadruplets that sum to zero.
-  int zeroQuadrupletsCount(int* a, int* b, int* c, int* d) {
+  int zeroQuadrupletsCount(int* a, int* b, int* c, int* d, int size) {
     // ...
   }
 };
