cd9108506c6695f83fb46ffa3be9e83746563fae
Stanislav Gatev <s2gatev@gmail.com>
2015-06-20 12:31:56 +0300

    Update Stack template

--- week1/3-Stack/stack.py
+++ week1/3-Stack/stack.py
@@ -1,21 +1,21 @@
 class Stack:
 
-    # Adds value to the end of the Queue.
+    # Adds value to the end of the Stack.
     # Complexity: O(1)
     def push(value):
         pass
 
-    # Returns value from the front of the Queue and removes it.
+    # Returns value from the end of the Stack and removes it.
     # Complexity: O(1)
     def pop():
         pass
 
-    # Returns value from the front of the Queue without removing it.
+    # Returns value from the end of the Stack without removing it.
     # Complexity: O(1)
     def peek():
         pass
 
-    # Returns the number of elements in the Queue.
+    # Returns the number of elements in the Stack.
     # Complexity: O(1)
     def size():
         pass
